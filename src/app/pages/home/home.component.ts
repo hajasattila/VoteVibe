ad1539bef023b5dc9c81102f96d91a26c96f213f
Attila <attila.hajas@procats.hu>
2025-04-06 14:13:17 +0200

    [MINOR] steps and rooms get separetad modal in dashboard

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import {AuthService} from 'src/api/services/auth-service/auth.service';
 import {Router} from "@angular/router";
 import {ThemeService} from "../../../api/services/theme-service/theme-service.service";
 import {TranslateService} from "@ngx-translate/core";
+import {Room} from "../../../api/models/room";
+import {UsersService} from "../../../api/services/users-service/users.service";
 
 @Component({
     selector: 'app-home',
@@ -11,12 +13,16 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class HomeComponent implements OnInit {
 
-    constructor(private readonly authService: AuthService, private readonly router: Router,
+    constructor(private readonly authService: AuthService,
+                private readonly router: Router,
                 protected themeService: ThemeService,
-                protected translate: TranslateService) {
+                protected translate: TranslateService,
+                private userService: UsersService,) {
     }
 
     protected menuOpen = false;
+    showRoomModal: boolean = false;
+    userRooms: Room[] = [];
 
 
     ngOnInit(): void {
@@ -48,6 +54,27 @@ export class HomeComponent implements OnInit {
 
     toggleSideMenu() {
         this.menuOpen = !this.menuOpen;
+    }
+
+    openRoomModal(): void {
+        this.showRoomModal = true;
+
+        this.authService.getCurrentUser().subscribe((user) => {
+            if (user) {
+                this.userService.getUserById(user.uid).subscribe((profileUser) => {
+                    this.userRooms = profileUser?.gameRooms || [];
+                });
+            }
+        });
+    }
+
+    closeRoomModal(): void {
+        this.showRoomModal = false;
+    }
+
+    navigateToRoom(room: Room): void {
+        this.closeRoomModal();
+        this.router.navigate(['/room', room.roomId]);
     }
 
 }
